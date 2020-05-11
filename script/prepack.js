6c314c86f65dd84f9972631d14ed7eab67da9fda
Dirk Lüth <dirk.lueth@gmail.com>
2020-05-11 18:02:13 +0200

    testing

--- script/prepack.js
+++ script/prepack.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const locations = require('../shared/locations.js');
 
 locations.public.forEach((location) => {
     const pkg = require(location + '/package.json');
-    console.log('=>', location, pkg);
+
+    pkg.main   = pkg.main.replace(/^dist\//, '');
+    pkg.module = pkg.module.replace(/^dist\//, '');
+
+    delete pkg.scripts;
+    delete pkg.publishConfig;
+
+    fs.writeFileSync(location + '/dist/package.json', JSON.stringify(pkg, null, 4))
 });
