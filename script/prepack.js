1d74419c62e23c75580481471661e2b4e505504f
Dirk Lüth <dirk.lueth@gmail.com>
2020-05-11 17:56:11 +0200

    testing

--- script/prepack.js
+++ script/prepack.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const locations = require('../shared/paths.js');
+const locations = require('../shared/locations.js');
 
 locations.public.forEach((location) => {
     console.log('=>', location);
