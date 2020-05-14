2bbc2c3e002df5728d2f1553c418c4ee3e16c23d
Dirk Lüth <dirk.lueth@gmail.com>
2020-05-14 20:55:29 +0200

    slight change in structure of @qoopido/utility

--- packages/utility/src/index.js
+++ packages/utility/src/index.js
@@ -1,16 +1,4 @@
-import global from "./constant/global";
-import doImmediate from "./helper/doImmediate";
-import toArray from "./helper/toArray";
-import Uuid from "./helper/uuid";
-
-var constant = {
-    global,
-};
-
-var helper = {
-    doImmediate,
-    toArray,
-    Uuid,
-};
+import * as constant from "./constant";
+import * as helper from "./helper";
 
 export { constant, helper };
