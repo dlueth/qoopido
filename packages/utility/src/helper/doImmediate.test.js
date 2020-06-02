88c6830caa2d9e49e47e29acc7226f1e0d0efedf
Dirk Lüth <dirk.lueth@gmail.com>
2020-06-02 10:51:39 +0200

    test(utility): fix tests for `helper/doImmediate`

--- packages/utility/src/helper/doImmediate.test.js
+++ packages/utility/src/helper/doImmediate.test.js
@@ -12,62 +12,68 @@ describe("doImmediate()", () => {
     });
 
     test("should successfully call the callback (setImmediate)", () => {
-        MutationObserver = undefined;
-        document = undefined;
+        return new Promise((resolve, reject) => {
+            MutationObserver = undefined;
+            document = undefined;
 
-        const doImmediate = require("../../dist/helper/doImmediate");
-        const callback = jest.fn();
+            const doImmediate = require("../../dist/helper/doImmediate");
+            const callback = jest.fn();
 
-        doImmediate(callback);
+            doImmediate(callback);
 
-        setTimeout(() => {
-            try {
-                expect(callback.mock.calls.length).toBe(1);
+            setTimeout(() => {
+                try {
+                    expect(callback.mock.calls.length).toBe(1);
 
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            });
         });
     });
 
     test("should successfully call the callback (MutationObserver)", () => {
-        setImmediate = undefined;
+        return new Promise((resolve, reject) => {
+            setImmediate = undefined;
 
-        const doImmediate = require("../../dist/helper/doImmediate");
-        const callback = jest.fn();
+            const doImmediate = require("../../dist/helper/doImmediate");
+            const callback = jest.fn();
 
-        doImmediate(callback);
+            doImmediate(callback);
 
-        setTimeout(() => {
-            try {
-                expect(callback.mock.calls.length).toBe(1);
+            setTimeout(() => {
+                try {
+                    expect(callback.mock.calls.length).toBe(1);
 
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            });
         });
     });
 
     test("should successfully call the callback (setTimeout)", () => {
-        setImmediate = undefined;
-        MutationObserver = undefined;
-        document = undefined;
+        return new Promise((resolve, reject) => {
+            setImmediate = undefined;
+            MutationObserver = undefined;
+            document = undefined;
 
-        const doImmediate = require("../../dist/helper/doImmediate");
-        const callback = jest.fn();
+            const doImmediate = require("../../dist/helper/doImmediate");
+            const callback = jest.fn();
 
-        doImmediate(callback);
+            doImmediate(callback);
 
-        setTimeout(() => {
-            try {
-                expect(callback.mock.calls.length).toBe(1);
+            setTimeout(() => {
+                try {
+                    expect(callback.mock.calls.length).toBe(1);
 
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
+            });
         });
     });
 });
