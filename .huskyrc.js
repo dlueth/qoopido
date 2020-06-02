e6f23c80d927d96e357df766ccfb66925048c3c2
Dirk Lüth <dirk.lueth@gmail.com>
2020-06-02 12:58:27 +0200

    chore: add commitizen to workflow

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,7 @@
 module.exports = {
     "hooks": {
         "pre-commit": "lint-staged --config ./.lintstagedrc.js && yarn test:coverage",
+        "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true",
         "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"
     }
 }
