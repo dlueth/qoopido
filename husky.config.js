d4b33ff22d55705f43e68dc15f7b2a62f7ceace7
Dirk Lüth <dirk.lueth@gmail.com>
2020-06-09 00:09:28 +0200

    fix: lint-staged config setting

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     "hooks": {
-        "pre-commit": "lint-staged --config ./.lintstagedrc.js && yarn test:coverage",
+        "pre-commit": "lint-staged && yarn test:coverage",
         "prepare-commit-msg": "exec < /dev/tty && git cz --hook || true",
         "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"
     }
