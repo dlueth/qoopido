029344eff558c1600a3550ebe2e8c2f8dd78e244
Dirk Lüth <dirk.lueth@gmail.com>
2020-06-19 14:04:52 +0200

    test: prevent jest running all tests when testing specific package

--- packages/emitter/jest.config.js
+++ packages/emitter/jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     displayName: name,
     name: name,
     rootDir: "../..",
+    projects: undefined,
     testMatch: [ `<rootDir>/packages/${dir}/src/**/*.test.js` ],
     collectCoverageFrom: [
         `<rootDir>/packages/${dir}/src/**/*.js`,
