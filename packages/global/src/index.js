fd9ec0ebff2fb7b43397554a53779d0d23c5852a
Dirk Lüth <dirk.lueth@gmail.com>
2020-06-23 15:38:42 +0200

    chore: add `@qooido/global` package

---
+++ packages/global/src/index.js
@@ -0,0 +1,14 @@
+let result;
+
+/* istanbul ignore else  */
+if (typeof globalThis === "object" && globalThis.globalThis === globalThis) {
+    result = globalThis;
+} else if (typeof self === "object" && self.self === self) {
+    result = self;
+} else if (typeof global === "object" && global.global === global) {
+    result = global;
+} else {
+    result = this;
+}
+
+export default result;
