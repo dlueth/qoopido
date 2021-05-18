0bbf7ab0302dd5d3e7eba610495a37a6768cd8f9
Dirk Lüth <dirk.lueth@gmail.com>
2021-05-18 17:40:35 +0200

    build: update dev-dependencies
    update dev-dependencies to latest available versions and auto-fix linting errors

--- packages/utility/src/helper/uuid.test.js
+++ packages/utility/src/helper/uuid.test.js
@@ -1,6 +1,7 @@
 import uuid from "./uuid";
 
-const matchUuid = /(?:^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}$)|(?:^0{8}-0{4}-0{4}-0{4}-0{12}$)/u;
+const matchUuid =
+    /(?:^[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}$)|(?:^0{8}-0{4}-0{4}-0{4}-0{12}$)/u;
 
 describe("Uuid()", () => {
     test("generated UUIDs should validate", () => {
--- packages/validator/src/isSemver.js
+++ packages/validator/src/isSemver.js
@@ -1,6 +1,7 @@
 import isString from "./isString";
 
-const regexMatchSemver = /^\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-]+(?:\.[\da-z-]+)*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?\b$/i;
+const regexMatchSemver =
+    /^\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-[\da-z-]+(?:\.[\da-z-]+)*)?(?:\+[\da-z-]+(?:\.[\da-z-]+)*)?\b$/i;
 
 /**
  * Check if value is a valid version (according to semver 2.0.0)
